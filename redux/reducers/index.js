e202d2f39129864770604548fb1ba838aa59dbf0
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-16 17:30:02 +0700

    add redux persist transform encrypt on auth reducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -4,15 +4,25 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import storage from "redux-persist/lib/storage";
 import emailVerificationReducer from "./emailVerificationReducer";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import session from "redux-persist/lib/storage/session";
 
+const encryptor = encryptTransform({
+  secretKey: "myKey",
+  onError: function (error) {
+    console.log(error);
+  },
+});
 const authPersistConfig = {
   key: "root",
   storage: storage,
+  transforms: [encryptor],
   blacklist: ["auth"],
 };
 const emailPersistConfig = {
   key: "email",
-  storage: storage,
+  storage: session,
+  transforms: [encryptor],
   blacklist: ["email"],
 };
 
