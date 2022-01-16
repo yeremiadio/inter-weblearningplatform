8b6b72b47fce5977b6caaaff7484376e801f0410
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-16 19:48:40 +0700

    logout remove window location

--- utils/logOut.js
+++ utils/logOut.js
@@ -1,11 +1,11 @@
 import { LOGOUT } from "../constants/types";
-import store from "../redux/store";
+import { store } from "../redux/store";
 
 export const logOut = () => {
   if (typeof window !== "undefined") {
     store.dispatch({
       type: LOGOUT,
     });
-    router.push("/login");
+    localStorage.clear();
   }
 };
