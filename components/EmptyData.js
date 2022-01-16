cbe9092db148c33541c9b785510d15faa7ab6c1c
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-16 17:18:42 +0700

    empty data component

---
+++ components/EmptyData.js
@@ -0,0 +1,26 @@
+import { Button } from "@chakra-ui/react";
+import { PlusIcon } from "@heroicons/react/solid";
+import { useRouter } from "next/router";
+
+function EmptyDataComponent({ title = "Data" }) {
+  const router = useRouter();
+  return (
+    <div className="z-50 flex flex-col justify-center items-center mt-16 lg:mt-24">
+      <img
+        src="../emptyState.svg"
+        alt=""
+        className="object-cover w-3/4 lg:w-1/5 mb-4"
+      />
+      <h3 className="font-bold text-2xl mb-6">Oops {title} kosong</h3>
+      <Button
+        colorScheme="blue"
+        leftIcon={<PlusIcon className="w-4 h-4" />}
+        onClick={() => router.push("materials/create")}
+      >
+        Tambah {title}
+      </Button>
+    </div>
+  );
+}
+
+export default EmptyDataComponent;
