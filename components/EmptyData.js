c225b78427bd46a0baffc47139dd13250cd41e03
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-18 12:55:37 +0700

    empty data with next router

--- components/EmptyData.js
+++ components/EmptyData.js
@@ -2,7 +2,7 @@ import { Button } from "@chakra-ui/react";
 import { PlusIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 
-function EmptyDataComponent({ title = "Data" }) {
+function EmptyDataComponent({ title = "Data", href = "/" }) {
   const router = useRouter();
   return (
     <div className="z-50 flex flex-col justify-center items-center mt-16 lg:mt-24">
@@ -15,7 +15,7 @@ function EmptyDataComponent({ title = "Data" }) {
       <Button
         colorScheme="blue"
         leftIcon={<PlusIcon className="w-4 h-4" />}
-        onClick={() => router.push("materials/create")}
+        onClick={() => router.push(href)}
       >
         Tambah {title}
       </Button>
