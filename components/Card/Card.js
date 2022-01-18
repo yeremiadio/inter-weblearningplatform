db59d80981ed4997557ad4c23f04aff161019db5
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-18 12:56:13 +0700

    added editable boolean on custom card component

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -3,9 +3,15 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
 
-const CustomCard = ({ thumbnail, name, description, slug }) => {
+const CustomCard = ({
+  thumbnail = "",
+  name = "",
+  description = "",
+  slug = "",
+  isEditable = false,
+}) => {
   return (
-    <div className="hover:shadow-lg transition-all delay-75 border border-gray-200 rounded-lg">
+    <div className="hover:shadow-default-weblearning transition-all delay-75 border border-gray-200 rounded-lg bg-white">
       <img
         src={thumbnail !== null ? thumbnail : "/imgPlaceholder.jpg"}
         alt=""
@@ -18,19 +24,23 @@ const CustomCard = ({ thumbnail, name, description, slug }) => {
         <p className="text-secondary leading-loose text-base line-clamp-3 my-2">
           {description}
         </p>
-        <Link href={slug ? slug : "/"}>
-          <a>
-            <Button
-              colorScheme={"blue"}
-              size="md"
-              colorScheme="blue"
-              className="p-6"
-              isFullWidth
-            >
-              Detail
-            </Button>
-          </a>
-        </Link>
+        {isEditable ? (
+          "Editable"
+        ) : (
+          <Link href={slug ? slug : "/"}>
+            <a>
+              <Button
+                colorScheme={"blue"}
+                size="sm"
+                colorScheme="blue"
+                variant="outline"
+                isFullWidth
+              >
+                Detail
+              </Button>
+            </a>
+          </Link>
+        )}
       </div>
     </div>
   );
