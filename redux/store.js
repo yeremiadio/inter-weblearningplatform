792bc36afe1b222ada09e759a14ee16e7ebd3650
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-03-05 17:11:37 +0700

    remove redux devtools production

--- redux/store.js
+++ redux/store.js
@@ -1,21 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import reducers from "./reducers/index";
 import { persistStore } from "redux-persist";
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+// import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
 // With Devtools
 
-const store = PHASE_DEVELOPMENT_SERVER
-  ? createStore(
-      reducers,
-      composeWithDevTools(
-        applyMiddleware(thunk)
-        // other store enhancers if any
-      )
-    )
-  : createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const initializeStore = (initialState = {}) => {
   return store;
